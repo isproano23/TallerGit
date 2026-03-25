e0793deb43d460453e179882d192d8126b3ac6df
Mateo Molina <robert.mateo.3012@gmail.com>
2026-03-24 22:05:39 -0500

    fase 3 integrante 2 scanner

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,15 +1,22 @@
 package com.main;
 
 import com.Billetera.Billetera;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-	
+		Scanner scanner = new Scanner(System.in);
 
 	    Billetera miBilletera = new Billetera("Juan Perez", 100.0); 
 	    miBilletera.mostrarSaldo(); 
-	    miBilletera.transferir(20.0); 
+
+		//preguntar cuanto transferir
+		System.out.println("¿Cuánto desea transferir? ");
+		double monto = scanner.nextDouble();
+
+	    miBilletera.transferir(monto); 
 	    miBilletera.mostrarSaldo(); 
+		 scanner.close();
 	    } 
 	    } 
