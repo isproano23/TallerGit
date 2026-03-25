392d325f9cfad3c93fe4cf0f0486ffa80dae80d1
Aantony <alexanderpillalaza1997@gmail.com>
2026-03-24 21:27:09 -0500

    resuelto merge

--- src/com/Billetera/Billetera.java
+++ src/com/Billetera/Billetera.java
@@ -14,11 +14,7 @@ public class Billetera {
 	    public void transferir(double monto) { 
 	        balanceTotal= balanceTotal- monto; 
 	    } 
-<<<<<<< HEAD
 	 // probar 
-=======
-	  // atributos
->>>>>>> 7c915cbf8151a3f12c4015f7b617abe1de4800a9
 	    public void mostrarSaldo() { 
 	        System.out.println("Saldo de " + titular + ": $" + balanceTotal); 
 	    } 
