b0ce5ab12bf5ba0cc8178ee61979e54d0515df64
Aantony <alexanderpillalaza1997@gmail.com>
2026-03-24 22:05:42 -0500

    fase 3 intrenagten 3

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -11,5 +11,11 @@ public class Main {
 	    miBilletera.mostrarSaldo(); 
 	    miBilletera.transferir(20.0); 
 	    miBilletera.mostrarSaldo(); 
+
+		Billetera billetera2 = new Billetera("Maria Lopez", 500.0);
+        billetera2.mostrarSaldo();
+
+        miBilletera.mostrarSaldo();
+
 	    } 
 	    } 
